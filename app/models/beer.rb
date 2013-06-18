68173d39c569348b0ba1c847ed3e3bc017c4f5d5
Keith Thompson <keith@brilliantfantastic.com>
2013-06-18 13:34:42 -0400

    Completed the refrigerator tests

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,7 +1,7 @@
 class Beer < ActiveRecord::Base
   attr_accessible :abv, :name, :name_confirmation
 
-  has_one :refrigerator
+  belongs_to :refrigerator
 
   validates :name, confirmation: true, presence: true, uniqueness: true
   validates :abv, format: { with: /^[0-9]{1,2}\.[0-9]{1,2}$/ }, allow_nil: true
