9f79c3c4e98393b72fe721347bb3db2ad8cf7b8e
Keith Thompson <keith@brilliantfantastic.com>
2013-06-18 12:20:53 -0400

    Wrote and Implemented confirmation test

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,5 @@
 class Beer < ActiveRecord::Base
-  attr_accessible :abv, :name
+  attr_accessible :abv, :name, :name_confirmation
+
+  validates :name, confirmation: true
 end
--- test/models/beer_test.rb
+++ test/models/beer_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 describe 'A new', Beer do
-  it 'requires confirmation of name'
+  it 'requires confirmation of name' do
+    beer = Beer.new(name: 'x', name_confirmation: '')
+    beer.valid?.must_equal false
+
+    beer.name_confirmation = 'y'
+    beer.valid?.must_equal false
+
+    beer.name_confirmation = 'x'
+    beer.valid?.must_equal true
+  end
+
   it 'is invalid if it has no name'
   it 'is valid if it has a name, confirmation, and no ABV'
   it 'is invalid if the name has already been taken'
