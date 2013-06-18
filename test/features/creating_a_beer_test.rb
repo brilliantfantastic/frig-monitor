99965eae858b3da8a3a05a32551b502941c58d49
Keith Thompson <keith@brilliantfantastic.com>
2013-06-18 12:33:09 -0400

    Added the invalid data feature tests

--- test/features/creating_a_beer_test.rb
+++ test/features/creating_a_beer_test.rb
@@ -29,4 +29,31 @@ feature "Creating A Beer Feature Test" do
       page.must_have_content beer.abv
     end
   end
+
+  describe "with invalid data" do
+    before do
+      Beer.create(name: 'Dogfather Imperial Stout', abv: 8.45)
+      visit new_beer_path
+    end
+
+    scenario "displays errors when submitting empty form" do
+      within('#new_beer') do
+        click_on 'Create Beer'
+      end
+
+      page.must_have_content "Name can't be blank"
+      current_path.must_equal '/beers'
+    end
+
+    scenario "displays error when beer name is already taken" do
+      within('#new_beer') do
+        fill_in 'Beer Name', with: 'Dogfather Imperial Stout'
+        fill_in 'Alcohol by Volume', with: 8.45
+        click_on 'Create Beer'
+      end
+
+      page.must_have_content 'Name has already been taken'
+      current_path.must_equal '/beers'
+    end
+  end
 end
