c381eab1320b5038fdb029fca4e520bdfd1701bd
Keith Thompson <keith@brilliantfantastic.com>
2013-06-18 12:29:44 -0400

    Wrote and implemented tests for abv

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,4 +2,5 @@ class Beer < ActiveRecord::Base
   attr_accessible :abv, :name, :name_confirmation
 
   validates :name, confirmation: true, presence: true, uniqueness: true
+  validates :abv, format: { with: /^[0-9]{1,2}\.[0-9]{1,2}$/ }, allow_nil: true
 end
--- test/models/beer_test.rb
+++ test/models/beer_test.rb
@@ -33,7 +33,24 @@ describe 'A new', Beer do
   end
 
   describe 'with a name' do
-    it 'is invalid if the ABV is not one or two digits followed by a period and up to 2 digits'
-    it 'is valid if the ABV if it is 2 digits followed by a period and 2 digits'
+    before do
+      @beer = Beer.new(name: 'x', name_confirmation: 'x')
+    end
+
+    it 'is invalid if the ABV is not one or two digits followed by a period and up to 2 digits' do
+      @beer.abv = 0.456 # 3 after period
+      @beer.valid?.must_equal false
+
+      @beer.abv = 100.45 # 3 before period
+      @beer.valid?.must_equal false
+    end
+
+    it 'is valid if the ABV if it is 2 digits followed by a period and 2 digits' do
+      @beer.abv = 10.45
+      @beer.valid?.must_equal true
+
+      @beer.abv = 1.1
+      @beer.valid?.must_equal true
+    end
   end
 end
