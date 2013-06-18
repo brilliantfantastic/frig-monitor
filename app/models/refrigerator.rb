c6f9e07583e6c6b837cb4a97221f718d13167e3a
Keith Thompson <keith@brilliantfantastic.com>
2013-06-18 12:44:11 -0400

    Moved the refrigerator model over to being an active record model

--- app/models/refrigerator.rb
+++ app/models/refrigerator.rb
@@ -1,29 +1,5 @@
 class Refrigerator
-  attr_reader :drinks
-  attr_accessor :capacity
+  attr_accessible :brand, :beers
 
-  def initialize(attrs = {})
-    @drinks = attrs[:drinks] ? attrs[:drinks] : []
-    @capacity = attrs[:capacity] ? attrs[:capacity] : 12
-  end
-
-  def count
-    @drinks.count
-  end
-
-  def full?
-    count == @capacity
-  end
-
-  def empty?
-    count == 0
-  end
-
-  def add_drink(drink)
-    @drinks << drink unless full?
-  end
-
-  def remove_drink
-    @drinks.delete_at(0) unless empty?
-  end
+  has_many :beers
 end
