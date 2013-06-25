380351a2553f5bab58e81e8632cc5a1054ec68c3
Keith Thompson <keith@brilliantfantastic.com>
2013-06-25 08:21:32 -0400

    Added /signin route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 FrigMonitor::Application.routes.draw do
   resources :beers, only: [:new, :create, :show]
   resources :users, only: [:new, :create]
+
+  match '/signin' => 'sessions#new'
+
   root :to => 'beverages#index'
 end
