c327463be837ad67cdc339e5e7def8e61916af56
Keith Thompson <keith@brilliantfantastic.com>
2013-06-25 08:29:06 -0400

    Set sorcery and User model to work together

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130625010851) do
+ActiveRecord::Schema.define(:version => 20130625122738) do
 
   create_table "beers", :force => true do |t|
     t.string   "name"
@@ -30,9 +30,10 @@ ActiveRecord::Schema.define(:version => 20130625010851) do
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+    t.string   "crypted_password"
+    t.string   "salt"
   end
 
 end
