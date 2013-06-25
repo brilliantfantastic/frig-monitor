7b9712d097ecc5908005c6604a542520fcfa4dd1
Keith Thompson <keith@brilliantfantastic.com>
2013-06-24 21:04:45 -0400

    Fixed issues with Refrigerator contents listing

--- app/controllers/beverages_controller.rb
+++ app/controllers/beverages_controller.rb
@@ -1,7 +1,7 @@
 class BeveragesController < ApplicationController
   def index
-    @frig = Refrigerator.new
-    @frig.add_drink Beer.new(name: 'Edmond Fitzgerald', abv: 4.8)
-    @frig.add_drink Beer.new(name: 'Hoppy IPA', abv: 8.9)
+    @frig = Refrigerator.create
+    @frig.beers << Beer.new(name: 'Edmond Fitzgerald', abv: 4.8)
+    @frig.beers << Beer.new(name: 'Hoppy IPA', abv: 8.9)
   end
 end
