8708e4984dd9dc32112af8506d2c56844c3eb744
Keith Thompson <keith@brilliantfantastic.com>
2013-06-25 08:24:57 -0400

    Created sign in form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 FrigMonitor::Application.routes.draw do
   resources :beers, only: [:new, :create, :show]
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:create]
 
   match '/signin' => 'sessions#new'
 
