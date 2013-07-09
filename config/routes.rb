05005ec3cf2cdd2e4abf943587089a3739cf3bf5
Keith Thompson <keith@brilliantfantastic.com>
2013-07-09 06:26:17 -0400

    Nested the beers routes inside of the friges routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 FrigMonitor::Application.routes.draw do
-  resources :beers, only: [:new, :create, :show]
   resources :users, only: [:new, :create]
   resources :sessions, only: [:create]
 
   match '/signin' => 'sessions#new'
   match '/signout' => 'sessions#destroy'
 
-  resources :friges, only: [:new, :create, :show]
+  resources :friges, only: [:new, :create, :show] do
+    resources :beers, only: [:new, :create, :show]
+  end
 
   root :to => 'beverages#index'
 end
