05aa647e43da10bf7cb215c6ca6c35b1b780e69f
Keith Thompson <keith@brilliantfantastic.com>
2013-07-09 07:00:59 -0400

    Wrote test for javascript creation of refrigerator and adjusted gems, and support code

--- test/features/creating_a_refrigerator_test.rb
+++ test/features/creating_a_refrigerator_test.rb
@@ -24,4 +24,29 @@ feature 'Creating A Refrigerator Feature Test' do
     current_path.must_equal '/friges'
     page.must_have_content "Brand can't be blank"
   end
+
+  describe 'while staying on the same page' do
+    self.use_transactional_fixtures = false
+
+    before do
+      visit kitchen_path
+    end
+
+    after(:each) do
+      User.destroy_all
+      Refrigerator.destroy_all
+    end
+
+    scenario 'creating a frig with valid data', js: true do
+      click_link 'Add refrigerator'
+      within '#new_refrigerator' do
+        fill_in 'refrigerator_brand', with: 'Samsung'
+        page.find('#refrigerator_brand').native.send_keys :return
+      end
+      sleep 1
+      new_frig = Refrigerator.last
+      current_path.must_equal kitchen_path
+      page.must_have_content new_frig.brand
+    end
+  end
 end
