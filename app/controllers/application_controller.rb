6ddc6bb95db27ac184372dead5d0fc9286c1eb6e
Keith Thompson <keith@brilliantfantastic.com>
2013-07-09 06:54:02 -0400

    Restrict access to viewing beers to only the owner of the frig

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def ensure_frig_visible(frig)
+    visible = Refrigerator.for(current_user).include? frig
+    not_found unless visible
+  end
+
+  def ensure_beer_in_frig(beer, frig)
+    in_frig = frig.beers.include? beer
+    not_found unless in_frig
+  end
+
+  def not_found
+    render file: 'public/404', status: :not_found and return
+  end
 end
--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,15 +1,22 @@
 class BeersController < ApplicationController
+  before_filter :require_login
+
   def show
+    frig = Refrigerator.find(params[:frige_id])
+    ensure_frig_visible frig
     @beer = Beer.find(params[:id])
+    ensure_beer_in_frig @beer, frig
   end
 
   def new
     @frig = Refrigerator.find(params[:frige_id])
+    ensure_frig_visible @frig
     @beer = Beer.new
   end
 
   def create
     @frig = Refrigerator.find(params[:frige_id])
+    ensure_frig_visible @frig
     @beer = @frig.beers.new(params[:beer])
     if @beer.save
       redirect_to frige_beer_path(@frig, @beer)
